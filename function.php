712223e09eee5df858f35e1df7baa62201c07942
Nipponvisions <nipponvisions@gmail.com>
2018-08-21 00:39:29 -0400

    sync with pc

---
+++ function.php
@@ -0,0 +1,7 @@
+<?php
+
+// Enqueuing Scripts and Styles 
+
+wp_enqueue_style( 'style', get_stylesheet_uri() );
+
+
---
+++ header.php
@@ -0,0 +1,7 @@
+<?php
+// Template name: header.php 
+
+
+// https://codex.wordpress.org/Designing_Headers
+
+
